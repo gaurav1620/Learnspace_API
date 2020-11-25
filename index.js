f4f4f995510285207cbf2d0cea82275e1ee096aa
gaurav1620 <gauravak007@gmail.com>
2020-11-25 15:14:41 +0530

    added POST and GET /students

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const mysql = require('mysql');
@@ -10,8 +12,46 @@ const db = mysql.createPool({
 
 })
 
+app.use(bodyParser.json());
+
+app.get('/deleteschema', (req, res) => {
+  db.query('DROP TABLE student;', (err, dat) => {
+    if(err)
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    return res.send({"success":true});
+  })
+})
+
+app.get('/updateschema', (req, res) => {
+  db.query('CREATE TABLE student (_id INT PRIMARY KEY AUTO_INCREMENT,fname VARCHAR(50) NOT NULL, lname VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL UNIQUE, password VARCHAR(50) NOT NULL, year VARCHAR(50) NOT NULL, department VARCHAR(50) NOT NULL);', (err, dat) => {
+    if(err)
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    return res.send({"success":true});
+  })
+})
+
+app.post('/students', (req, res) => {
+  console.log(req.body);
+  const query = `INSERT INTO student(fname, lname, email, password, year, department) VALUES('${req.body.fname}', '${req.body.lname}', '${req.body.email}', '${req.body.password}', '${req.body.year}', '${req.body.department}');`;
+  console.log(query);
+  db.query(query, (err, data) => {
+    if(err)
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    return res.send({"success":true, "data" : data});
+  })
+})
+
+app.get('/students', (req, res) => {
+  const query = 'SELECT * FROM student';
+  db.query(query, (err, data) => {
+    if(err)
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    return res.send({"success":true, "data" : data});
+  })
+})
+
 app.get('/', (req, res) => {
-  db.query('SELECT * FROM student', (err, dat) => {
+  db.query('SELECT * FROM student;', (err, dat) => {
     if(err){
       console.log(err);
       return res.status(400).send({"error" : "Error occured during exection of SQL query", "name": err.name, "desc" : err.description});
