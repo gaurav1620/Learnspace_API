644f0b468ee638dc3a23c4093c6a9c60d83c1a8b
gaurav1620 <gauravak007@gmail.com>
2020-11-30 20:24:29 +0530

    added new DB credentials

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
-module.exports.user = 'b5bd55a728c04c'
+module.exports.user = 'b87fe6b7ea29e2'
 module.exports.host = 'us-cdbr-east-02.cleardb.com'
-module.exports.password = 'd3895006'
-module.exports.database = 'heroku_93cc4493cfbc7cc'
+module.exports.password = '982368df'
+module.exports.database = 'heroku_d4f9118bf3983ba'
