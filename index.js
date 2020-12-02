65db40f200fbe037c9edec9f95418897624254d8
Yatish Kelkar <yatish1606@gmail.com>
2020-12-02 12:30:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ app.get('/course/:id', (req,res) => {
 
 //course existence check
 app.post('/course_check', (req, res) => {
-  const query = `SELECT * FROM course WHERE course_code=${req.body.course_code};`;
+  const query = `SELECT * FROM course WHERE course_code='${req.body.course_code}';`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
