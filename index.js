f4b0099b967e23e16e6fa7a1b998f86cdaa171cf
MorphTail <chitniskshitij@gmail.com>
2020-12-02 16:16:12 +0530

    course code check error handled

--- index.js
+++ index.js
@@ -200,8 +200,12 @@ app.post('/course', (req, res) => {
   const query = `INSERT INTO course(teacher_id, name, description, year, department, course_code)\
                  VALUES(${req.body.teacher_id}, '${req.body.name}', '${req.body.description}', '${req.body.year}', '${req.body.department}', '${req.body.course_code}');`;
   db.query(query, (err, data) => {
-    if(err)
-      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    if(err){
+      if(err.message.includes('ER_DUP_ENTRY')){
+        return res.send({"success":false, "reason": "course code exists"});
+      }
+      else return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    }
     return res.send({"success":true, "data" : data});
   })
 })
