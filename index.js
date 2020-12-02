cb52a8227f92c64cb8494824e7f85a585db4f231
gaurav1620 <gauravak007@gmail.com>
2020-12-02 15:12:19 +0530

    /records/:course_id now returns info of the students enrolled for that course and not just their id

--- index.js
+++ index.js
@@ -249,8 +249,8 @@ app.get('/records', (req, res) => {
   })
 })
 
-app.get('/coursesenrolled/:student_id', (req,res) => {
-  const query = `SELECT * from course WHERE _id IN(SELECT course_id FROM records WHERE student_id=${req.params.student_id});`;
+app.get('/records/:course_id', (req,res) => {
+  const query = `SELECT * FROM student WHERE _id IN(SELECT student_id FROM records WHERE course_id=${req.params.course_id});`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
@@ -258,8 +258,9 @@ app.get('/coursesenrolled/:student_id', (req,res) => {
   })
 })
 
-app.get('/records/:course_id', (req,res) => {
-  const query = `SELECT * FROM records WHERE course_id=${req.params.course_id}`;
+
+app.get('/coursesenrolled/:student_id', (req,res) => {
+  const query = `SELECT * from course WHERE _id IN(SELECT course_id FROM records WHERE student_id=${req.params.student_id});`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
