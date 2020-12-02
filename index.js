4d673f5af7ae99f1e12a62e254dc474e0a09e590
MorphTail <chitniskshitij@gmail.com>
2020-12-02 16:06:46 +0530

    better error handling 2

--- index.js
+++ index.js
@@ -154,8 +154,12 @@ app.post('/teacher', (req, res) => {
   const query = `INSERT INTO teacher(fname, lname, email, password) VALUES('${req.body.fname}', '${req.body.lname}', '${req.body.email}', '${req.body.password}');`;
   console.log(query);
   db.query(query, (err, data) => {
-    if(err)
-      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    if(err){
+      if(err.message.includes('ER_DUP_ENTRY')){
+        return res.send({"success":false, "reason": "Email exists"});
+      }
+      else return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    }
     return res.send({"success":true, "data" : data});
   })
 })
