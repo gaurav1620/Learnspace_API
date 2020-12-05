5abc0a3a6201dec0e065f93076c74a6f53c39775
MorphTail <47940274+m0rphtail@users.noreply.github.com>
2020-12-05 21:59:22 +0530

    remove log

--- index.js
+++ index.js
@@ -487,7 +487,6 @@ app.get('/describe/:tablename', (req, res)=>{
 
 app.get('/get_report/:assignment_id', (req, res)=>{
   const query =`CALL send_report('${req.param.assignment_id}')`;
-  console.log(query);
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
