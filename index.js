059d62b993fdb6440593120c35098ab636e8e9f9
gaurav1620 <gauravak007@gmail.com>
2020-12-06 20:58:31 +0530

    fixing /remove_from_course

--- index.js
+++ index.js
@@ -315,7 +315,7 @@ app.post('/course_check', (req, res) => {
 
 //remove student from course
 app.post('/remove_from_course', (req, res) => {
-  const query = `delete from records where student_id='${req.body.student_id}' and course_id='${req.body.course_id}';`;
+  const query = `DELETE FROM records WHERE student_id=${req.body.student_id} AND course_id=${req.body.course_id}; DELETE FROM submissions WHERE student_id=${req.body.student_id} AND assignment_id IN (SELECT _id from assignments WHERE course_id=${req.params.course_id})`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
