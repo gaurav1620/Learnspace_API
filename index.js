866b970ebfc0555a59f3f5a4d063f87dc059ec81
Yatish Kelkar <yatish1606@gmail.com>
2020-12-06 16:34:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -525,8 +525,8 @@ app.get('/notes', (req, res) => {
   })
 })
 
-app.get('/notes/:id', (req,res) => {
-  const query = `SELECT * FROM notes WHERE user_id=${req.params.id};`;
+app.get('/notes/:user_type/:id', (req,res) => {
+  const query = `SELECT * FROM notes WHERE user_id=${req.params.id} and user_type='${req.params.user_type}';`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
