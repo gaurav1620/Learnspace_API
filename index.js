073334a1e93cabef0ba0ae9cdf2f042b2bcabd6c
Yatish Kelkar <yatish1606@gmail.com>
2020-12-06 16:37:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -506,8 +506,8 @@ app.get('/get_report/:assignment_id', (req, res)=>{
 })
 
 app.post('/notes', (req, res) => {
-  const query = `INSERT INTO notes(user_id, day, date, time, content)\
-                 VALUES(${req.body.user_id}, '${req.body.day}', '${req.body.date}', '${req.body.time}', '${req.body.content}');`;
+  const query = `INSERT INTO notes(user_id, user_type, day, date, time, content)\
+                 VALUES(${req.body.user_id}, '${req.body.user_type}', '${req.body.day}', '${req.body.date}', '${req.body.time}', '${req.body.content}');`;
   db.query(query, (err, data) => {
     if(err){    
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
