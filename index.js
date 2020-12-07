8b8a5d8034036467a2613da7b9600692cc37108b
MorphTail <chitniskshitij@gmail.com>
2020-12-08 00:50:00 +0530

    do counting

--- index.js
+++ index.js
@@ -337,7 +337,11 @@ app.post('/course_check', (req, res) => {
 
 //remove student from course
 app.post('/remove_from_course', (req, res) => {
-  const query = `DELETE FROM records WHERE student_id='${req.body.student_id}' AND course_id='${req.body.course_id}'; DELETE FROM submissions WHERE student_id='${req.body.student_id}' AND assignment_id IN (SELECT _id from assignment WHERE course_id='${req.params.course_id}');`;
+  const query = `
+  DELETE FROM records WHERE student_id='${req.body.student_id}' AND course_id='${req.body.course_id}';
+  DELETE FROM submissions WHERE student_id='${req.body.student_id}' AND assignment_id IN (SELECT _id from assignment WHERE course_id='${req.params.course_id}');
+  CALL UpdateStudentCount('${req.body.course_id}');
+  `;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
@@ -351,7 +355,8 @@ app.post('/remove_from_course', (req, res) => {
 //join course
 app.post('/records', (req, res) => {
   const query = `INSERT INTO records(student_id, course_id)\
-                 VALUES(${req.body.student_id}, ${req.body.course_id});`;
+                 VALUES(${req.body.student_id}, ${req.body.course_id});
+                 CALL UpdateStudentCount('${req.body.course_id}');`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
