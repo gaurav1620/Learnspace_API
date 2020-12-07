e48cd09ef42912ac176518767655f697a271982a
MorphTail <47940274+m0rphtail@users.noreply.github.com>
2020-12-07 21:45:17 +0530

    add '

--- index.js
+++ index.js
@@ -337,7 +337,7 @@ app.post('/course_check', (req, res) => {
 
 //remove student from course
 app.post('/remove_from_course', (req, res) => {
-  const query = `DELETE FROM records WHERE student_id=${req.body.student_id} AND course_id=${req.body.course_id}; DELETE FROM submissions WHERE student_id=${req.body.student_id} AND assignment_id IN (SELECT _id from assignments WHERE course_id=${req.params.course_id})`;
+  const query = `DELETE FROM records WHERE student_id='${req.body.student_id}' AND course_id='${req.body.course_id}'; DELETE FROM submissions WHERE student_id='${req.body.student_id}' AND assignment_id IN (SELECT _id from assignments WHERE course_id='${req.params.course_id}');`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
