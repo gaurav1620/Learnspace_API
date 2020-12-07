ce85f4af4e2e93bbbc185a8537cd937cd3689132
Yatish Kelkar <yatish1606@gmail.com>
2020-12-07 09:10:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -160,6 +160,21 @@ app.post('/student_login', (req, res) => {
   })
 })
 
+app.post('/updatestudentname', (req, res) => {
+  console.log(req.body);
+  const query = `UPDATE student SET fname=${req.body.fname}, lname=${req.body.lname} WHERE email=${req.body.email};`
+  console.log(query);
+  db.query(query, (err, data) => {
+    if(err){
+      if(err.message.includes('ER_DUP_ENTRY')){
+        return res.send({"success":false, "reason": "Email exists"});
+      }
+      else return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    }
+    return res.send({"success":true, "data" : data});
+  })
+})
+
 
 //TEACHER
 app.post('/teacher', (req, res) => {
@@ -179,7 +194,7 @@ app.post('/teacher', (req, res) => {
 
 app.post('/updateteachername', (req, res) => {
   console.log(req.body);
-  const query = `UPDATE teacher SET fname=${req.body.fname}, lname=${req.body.lname} WHERE email=${req.body.email} AND password=${req.body.password};`
+  const query = `UPDATE teacher SET fname=${req.body.fname}, lname=${req.body.lname} WHERE email=${req.body.email};`
   console.log(query);
   db.query(query, (err, data) => {
     if(err){
