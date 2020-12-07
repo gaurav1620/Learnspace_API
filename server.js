dc7689d1694ef02dffd448985145f91a43042d6b
MorphTail <chitniskshitij@gmail.com>
2020-12-07 23:26:27 +0530

    slowly disintegrating

--- server.js
+++ server.js
@@ -144,47 +144,17 @@ app.get('/marks/:assignment_id', (req, res) => {
 
 
 //remove student from course
-app.post('/remove_from_course', (req, res) => {
-  const query = `DELETE FROM records WHERE student_id=${req.body.student_id} AND course_id=${req.body.course_id};
-                 DELETE FROM submissions WHERE student_id=${req.body.student_id} AND assignment_id IN (SELECT _id from assignments WHERE course_id=${req.params.course_id})`;
-  db.query(query, (err, data) => {
-    if(err)
-      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
-    return res.send({"success":true, "data" : data});
-  })
-})
+
 
 
 //RECORDS
 
 //join course
-app.post('/records', (req, res) => {
-  const query = `INSERT INTO records(student_id, course_id)\
-                 VALUES(${req.body.student_id}, ${req.body.course_id});`;
-  db.query(query, (err, data) => {
-    if(err)
-      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
-    return res.send({"success":true, "data" : data});
-  })
-})
 
-app.get('/records', (req, res) => {
-  const query = 'SELECT * FROM records;';
-  db.query(query, (err, data) => {
-    if(err)
-      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
-    return res.send({"success":true, "data" : data});
-  })
-})
 
-app.get('/records/:course_id', (req,res) => {
-  const query = `SELECT * FROM student WHERE _id IN(SELECT student_id FROM records WHERE course_id=${req.params.course_id});`;
-  db.query(query, (err, data) => {
-    if(err)
-      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
-    return res.send({"success":true, "data" : data});
-  })
-})
+
+
+
 
 
 app.get('/coursesenrolled/:student_id', (req,res) => {
