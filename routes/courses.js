80b2890c9357680bd055a3315e69b1a55022eb77
MorphTail <chitniskshitij@gmail.com>
2020-12-07 23:26:40 +0530

    add stuff

--- routes/courses.js
+++ routes/courses.js
@@ -84,4 +84,15 @@ module.exports = function(app, db) {
           return res.send({"success":true, "data" : data});
         })
       })
+
+      //remove student from course
+      app.post('/remove_from_course', (req, res) => {
+        const query = `DELETE FROM records WHERE student_id=${req.body.student_id} AND course_id=${req.body.course_id};
+                       DELETE FROM submissions WHERE student_id=${req.body.student_id} AND assignment_id IN (SELECT _id from assignments WHERE course_id=${req.params.course_id})`;
+        db.query(query, (err, data) => {
+          if(err)
+            return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+          return res.send({"success":true, "data" : data});
+        })
+      })
 }
