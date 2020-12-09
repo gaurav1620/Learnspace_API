d37a867b03fb7094cf75828ebe6643dd95fd1751
gaurav1620 <gauravak007@gmail.com>
2020-12-09 10:07:01 +0530

    update

--- index.js
+++ index.js
@@ -644,9 +644,9 @@ app.get('/submissions/:assignment_id/:student_id', (req,res) => {
   })
 }) 
 
-app.post('/gradesubmission/:sub_id', (req,res) => {
+app.post('/gradesubmission/:assignment_id/:student_id', (req,res) => {
   console.log("marks : ",req.body.marks);
-  const query = `UPDATE submissions SET marks_obtained=${req.body.marks} WHERE _id=${req.params.sub_id};`;
+  const query = `UPDATE submissions SET marks_obtained=${req.body.marks} WHERE assignment_id=${req.params.assignment_id} AND student_id=${req.params.student_id};`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
