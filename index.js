575106326aac20940eb659f40710e31d61c5fc43
gaurav1620 <gauravak007@gmail.com>
2020-12-09 13:28:46 +0530

    update

--- index.js
+++ index.js
@@ -447,7 +447,7 @@ app.get('/hasattachedfile/:assignment_id', (req,res) => {
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
-    if(data || !data[0] || !data[0].filename )
+    if(!data || !data[0] || !data[0].filename )
       return res.status(200).send({"success":true, "file_exists": false});
     const pathh = __dirname + '/attachments/'+data[0].filename;
     if(!fs.existsSync(pathh))
