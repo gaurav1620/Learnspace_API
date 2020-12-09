ba8315d9ad0d95905280336861e69abcdbdc9935
gaurav1620 <gauravak007@gmail.com>
2020-12-09 12:19:00 +0530

    update

--- index.js
+++ index.js
@@ -442,9 +442,8 @@ app.get('/assignment/:course_id', (req,res) => {
   })
 }) 
 
-app.get('/hasattachedfile/:attachment_id', (req,res) => {
-  const query = `SELECT * FROM attachments WHERE _id=${req.params.attachment_id};`;
-  
+app.get('/hasattachedfile/:assignment_id', (req,res) => {
+  const query = `SELECT * FROM attachments WHERE assignment_id=${req.params.assignment_id};`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
