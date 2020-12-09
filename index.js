c2187172e4f263d8d522972f02222e81b10e3687
gaurav1620 <gauravak007@gmail.com>
2020-12-09 12:05:26 +0530

    adding GET /hassubmittedfile/:assignment_id/:student_id Which returns {"success: Bool, ""file_exists" : Bool} as a response

--- index.js
+++ index.js
@@ -581,6 +581,22 @@ app.post('/submissions/:assignment_id/:student_id',upload.single('train'), (req,
   })
 })
 
+app.get('/hassubmittedfile/:assignment_id/:student_id', (req,res) => {
+  //res.sendFile(path.join(__dirname, 'uploads', 'test.txt'))
+  const query = `SELECT * FROM submissions WHERE assignment_id=${req.params.assignment_id} AND student_id=${req.params.student_id};`;
+  db.query(query, (err, data) => {
+    if(err)
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+
+    const pathh = __dirname + '/submissions/'+data[0].filename;
+    if(!data || !data[0] || !data[0].filename || !fs.existsSync(pathh))
+      return res.status(200).send({"success":true, "file_exists": false});
+    return res.status(200).send({"success":true, "file_exists": true});
+    //return res.send({"success":true, "data" : data});
+  })
+  //res.download(__dirname + '/uploads/'+req.params.filename);
+})
+
 app.get('/getsubmittedfile/:assignment_id/:student_id', (req,res) => {
   //res.sendFile(path.join(__dirname, 'uploads', 'test.txt'))
   const query = `SELECT * FROM submissions WHERE assignment_id=${req.params.assignment_id} AND student_id=${req.params.student_id};`;
