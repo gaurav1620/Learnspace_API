61cbe6e507b87a4561dab8ce5bb24b6354c772c2
gaurav1620 <gauravak007@gmail.com>
2020-12-09 12:13:57 +0530

    update

--- index.js
+++ index.js
@@ -442,15 +442,40 @@ app.get('/assignment/:course_id', (req,res) => {
   })
 }) 
 
-
-app.get('/getattachedfile/:attachment_id', (req,res) => {
-  //res.sendFile(path.join(__dirname, 'uploads', 'test.txt'))
+app.get('/hasattachedfile/:attachment_id', (req,res) => {
   const query = `SELECT * FROM attachments WHERE _id=${req.params.attachment_id};`;
   
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    const pathh = __dirname + '/submissions/'+data[0].filename;
+    if(!data || !data[0] || !data[0].filename || !fs.existsSync(pathh))
+      return res.status(200).send({"success":true, "file_exists": false});
+    return res.status(200).send({"success":true, "file_exists": true});
+    //return res.send({"success":true, "data" : data});
+  })
+})
+
+
+app.get('/getattachedfile/:attachment_id', (req,res) => {
+  //res.sendFile(path.join(__dirname, 'uploads', 'test.txt'))
+  const query = `SELECT * FROM attachments WHERE _id=${req.params.attachment_id};`;
+  db.query(query, (err, data) => {
+    if(err)
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
     res.download(__dirname + '/attachemnts/'+data[0].filename);
+    //return res.send({"success":true, "data" : data});
+  })
+  //res.download(__dirname + '/uploads/'+req.params.filename);
+})
+
+app.get('/attachemnts', (req,res) => {
+  //res.sendFile(path.join(__dirname, 'uploads', 'test.txt'))
+  const query = `SELECT * FROM attachments;`;
+  db.query(query, (err, data) => {
+    if(err)
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    return res.status(200).send({"success":true, "data": data});
     //return res.send({"success":true, "data" : data});
   })
   //res.download(__dirname + '/uploads/'+req.params.filename);
