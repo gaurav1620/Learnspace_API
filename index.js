44888cdbbfd9423c5bd05b77195e069d1ba06721
Yatish Kelkar <yatish1606@gmail.com>
2020-12-14 13:55:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -387,12 +387,9 @@ app.get('/records/:course_id', (req,res) => {
   })
 })
 
-app.get('/search', (req,res) => {
-  res.send({'search':'bar'})
-})
 
 app.get('/search/:course_id/:fname/:lname', (req,res) => {
-  const query = `SELECT * FROM student WHERE _id IN(SELECT student_id FROM records WHERE course_id=${req.params.course_id}) AND (fname='${req.params.fname}' OR lname='${req.params.lname}');`;
+  const query = `SELECT * FROM student WHERE _id IN(SELECT student_id FROM records WHERE course_id=${req.params.course_id}) AND (fname='${req.params.fname}' AND lname='${req.params.lname}');`;
   db.query(query, (err, data) => {
     if(err)
       return res.status(400).send({"success":false, "error":err.name, "message": err.message});
