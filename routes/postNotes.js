ae92ad2edf896e5a1006c588070f31eba0643715
MorphTail <chitniskshitij@gmail.com>
2021-04-30 23:45:03 +0530

    fix get and post notes

--- routes/postNotes.js
+++ routes/postNotes.js
@@ -1,10 +1,10 @@
-module.exports = (app, db) => {
-  app.get('/notes', (req, res) => {
-    const query = `SELECT * FROM notes;`;
-    db.query(query, (err, data) => {
-      if(err)
-        return res.status(400).send({"success":false, "error":err.name, "message": err.message});
-      return res.send({"success":true, "data" : data});
-    })
+app.post('/notes', (req, res) => {
+  const query = `INSERT INTO notes(user_id, user_type, day, date, time, content)\
+                 VALUES(${req.body.user_id}, '${req.body.user_type}', '${req.body.day}', '${req.body.date}', '${req.body.time}', '${req.body.content}');`;
+  db.query(query, (err, data) => {
+    if(err){    
+      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+    }
+    return res.send({"success":true, "data" : data});
   })
-}
+})
