0661b9e193f65546ff090826da3dbac3e737065d
MorphTail <chitniskshitij@gmail.com>
2021-04-30 23:52:58 +0530

    fix get and post notes

---
+++ routes/getNotes.js
@@ -0,0 +1,11 @@
+module.exports = (app, db) => {
+    app.get('/notes', (req, res) => {
+      const query = `SELECT * FROM notes;`;
+      db.query(query, (err, data) => {
+        if(err)
+          return res.status(400).send({"success":false, "error":err.name, "message": err.message});
+        return res.send({"success":true, "data" : data});
+      })
+    })
+  }
+  
