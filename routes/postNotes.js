0672851c3d96148bf05a97271dad150b2198edff
MorphTail <chitniskshitij@gmail.com>
2021-04-30 23:56:08 +0530

    export

--- routes/postNotes.js
+++ routes/postNotes.js
@@ -1,10 +1,14 @@
-app.post('/notes', (req, res) => {
-  const query = `INSERT INTO notes(user_id, user_type, day, date, time, content)\
+module.exports = (app, db) => {
+  app.post("/notes", (req, res) => {
+    const query = `INSERT INTO notes(user_id, user_type, day, date, time, content)\
                  VALUES(${req.body.user_id}, '${req.body.user_type}', '${req.body.day}', '${req.body.date}', '${req.body.time}', '${req.body.content}');`;
-  db.query(query, (err, data) => {
-    if(err){    
-      return res.status(400).send({"success":false, "error":err.name, "message": err.message});
-    }
-    return res.send({"success":true, "data" : data});
-  })
-})
+    db.query(query, (err, data) => {
+      if (err) {
+        return res
+          .status(400)
+          .send({ success: false, error: err.name, message: err.message });
+      }
+      return res.send({ success: true, data: data });
+    });
+  });
+};
